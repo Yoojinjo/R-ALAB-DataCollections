e876cd747d42d2688471faf9a09816c9ab9e86b1
Yoojinjo <eugenejo111@gmail.com>
2024-08-07 21:12:37 -0500

    part 2

--- index.js
+++ index.js
@@ -11,3 +11,20 @@ for (let row of rows) {
     [cell1, cell2, cell3, cell4] = [...cells];
     console.log(cell1, cell2, cell3, cell4);
 }
+    //Part2. Expanding Functionality
+    // Declare a variable that stores the number of columns in each row of data within the CSV.
+let columns 
+
+// Instead of hard-coding four columns per row, expand your code to accept any number of columns. This should be calculated dynamically based on the first row of data.
+//Store your results in a two-dimensional array.
+// Each row should be its own array, with individual entries for each column.
+// Each row should be stored in a parent array, with the heading row located at index 0.
+// Cache this two-dimensional array in a variable for later use.
+columns = csv.split("\n");
+let data = []
+let data2 = []
+for (let i=0; i<columns.length; i++) {
+     data = columns[i].split(",")
+     console.log(data)
+    
+}
